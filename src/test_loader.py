ea5ceae4add89a2734cdac06eaa9518005203e0d
Rina Kadishani <kadishanirina@gmail.com>
2025-11-21 18:18:39 +0100

    Started the procedure of building the CNN model

--- src/test_loader.py
+++ src/test_loader.py
@@ -3,6 +3,6 @@ from src.data_loader import create_dataloaders
 train, val, test = create_dataloaders("data/DUTS")
 
 for imgs, masks in train:
-    print("Images:", imgs.shape)   # Expect [8, 3, 128, 128]
-    print("Masks:", masks.shape)   # Expect [8, 1, 128, 128]
+    print("Images:", imgs.shape)   
+    print("Masks:", masks.shape)  
     break
