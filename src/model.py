adbf75935efbd574a238a5a7b4885e9e597739cb
Rina Kadishani <kadishanirina@gmail.com>
2025-11-21 18:39:15 +0100

    Finished also the decoder part. 2 encoder blocks and 3 decoder blocks, ReLu activation and the 1 channel sigmoid output

--- src/model.py
+++ src/model.py
@@ -24,3 +24,48 @@ class CNN_SOD(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.pool3 = nn.MaxPool2d(2)
+
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.dec2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.up3 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.dec3 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.out_conv = nn.Conv2d(16, 1, kernel_size=1)
+
+    def forward(self, x):
+
+        x = self.enc1(x)
+        x = self.pool1(x)
+
+        x = self.enc2(x)
+        x = self.pool2(x)
+
+        x = self.enc3(x)
+        x = self.pool3(x)
+
+        x = self.up1(x)
+        x = self.dec1(x)
+
+        x = self.up2(x)
+        x = self.dec2(x)
+
+        x = self.up3(x)
+        x = self.dec3(x)
+
+        x = self.out_conv(x)
+        return torch.sigmoid(x)
+
