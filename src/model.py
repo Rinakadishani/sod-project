056b0ca79e429997fc1d0195d2b15455d8d23571
Rina Kadishani <kadishanirina@gmail.com>
2025-11-25 13:35:29 +0100

    Enhanced model while adding double conv layers and BatchNorm in each encoder/decoder block

--- src/model.py
+++ src/model.py
@@ -1,71 +1,73 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+def conv_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+    )
 
 
 class CNN_SOD(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.enc1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.enc1 = conv_block(3, 32)
         self.pool1 = nn.MaxPool2d(2)
 
-        self.enc2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.enc2 = conv_block(32, 64)
         self.pool2 = nn.MaxPool2d(2)
 
-        self.enc3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.enc3 = conv_block(64, 128)
         self.pool3 = nn.MaxPool2d(2)
 
+        self.enc4 = conv_block(128, 256)
+        self.pool4 = nn.MaxPool2d(2)
 
-        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec1 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.up4 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec4 = conv_block(128, 128)
+
+        self.up3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec3 = conv_block(64, 64)
 
         self.up2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.dec2 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.dec2 = conv_block(32, 32)
 
-        self.up3 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
-        self.dec3 = nn.Sequential(
-            nn.Conv2d(16, 16, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
+        self.up1 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.dec1 = conv_block(16, 16)
 
         self.out_conv = nn.Conv2d(16, 1, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+        x1 = self.enc1(x)
+        x = self.pool1(x1)
 
-        x = self.enc1(x)
-        x = self.pool1(x)
+        x2 = self.enc2(x)
+        x = self.pool2(x2)
 
-        x = self.enc2(x)
-        x = self.pool2(x)
+        x3 = self.enc3(x)
+        x = self.pool3(x3)
 
-        x = self.enc3(x)
-        x = self.pool3(x)
+        x4 = self.enc4(x)
+        x = self.pool4(x4)
 
-        x = self.up1(x)
-        x = self.dec1(x)
-
-        x = self.up2(x)
-        x = self.dec2(x)
+        x = self.up4(x)
+        x = self.dec4(x)
 
         x = self.up3(x)
         x = self.dec3(x)
 
-        x = self.out_conv(x)
-        return torch.sigmoid(x)
+        x = self.up2(x)
+        x = self.dec2(x)
 
+        x = self.up1(x)
+        x = self.dec1(x)
+
+        x = self.out_conv(x)
+        return self.sigmoid(x)
